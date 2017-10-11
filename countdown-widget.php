7881c0697e30c4e4c816adc5b1db6d6ac8f2cf37
Metin Şaylan <metinsaylan@gmail.com>
2017-10-11 23:55:51 +0300

    remove init file

--- countdown-widget.php
+++ countdown-widget.php
@@ -1,4 +1,16 @@
 <?php
+/*
+* Plugin Name: Countdown Widget
+* Plugin URI: http://metinsaylan.com/projects/wordpress/countdown-widget/
+* Description: A Beautiful jQuery Countdown Widget. Allows Multiple instances, Shortcode usage, and Customizations. Powered by: <a href="http://metinsaylan.com">metinsaylan</a>.
+* Version: 2.6.3
+* Author: Metin Saylan
+* Author URI: http://metinsaylan.com/
+* Text Domain: countdown-widget
+*/
+
+include_once('countdown-util.php'); // Utility functions for backward compatibility
+include_once('countdown-shortcodes.php'); // Shortcode functions
 
 global $countdown_shortcode_ids;
 
